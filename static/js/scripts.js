8092fd517736d4082196d27fbd7084c285149ca7
eunmiyo <good3103@gmail.com>
2023-02-02 20:19:41 +0900

    up

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -67,3 +67,9 @@
     }
   });
 })(jQuery);
+
+$(".que").click(function () {
+  $(this).next(".anw").stop().slideToggle(300);
+  $(this).toggleClass("on").siblings().removeClass("on");
+  $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
+});
